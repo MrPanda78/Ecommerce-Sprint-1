0cf1e228a842d03b09b21189a97c35948584c0b9
MrPanda78 <santidj28@gmail.com>
2026-04-16 14:02:41 -0300

    Added product page
    Added new page for /product to view information about products

--- app.js
+++ app.js
@@ -34,8 +34,39 @@ app.get("/", (req, res) => {
     res.render("pages/index", { loggedIn: 1, interests, mostRequested });
 });
 
-app.get("/products", (req, res) => {
-    res.render("pages/products", { loggedIn: 1 });
+app.get("/product", (req, res) => {
+
+    const product = {
+        name: "Whiskey Jack Daniels Honey 750ml",
+        price: "19.900",
+        images: [
+            "/images/whisky.webp",
+            "/images/whisky.webp",
+            "/images/whisky.webp"
+        ],
+        description: `Un verdaderamente fabuloso licor de whisky Blenders Pride. Está hecho con una mezcla de ricas especias y suave, miel tersa y el resultado es delicioso sobre hielo o café.
+
+        Aroma de caramelo y roble carbonizado, un poco de flor naranja, miel de manuka y vainilla.
+
+        Boca: grueso y cremoso, con notas de vainilla, roble tostado, miel de nuevo, un poco de albaricoque también.
+
+        Acabado: De buena longitud - redondeado y rico.
+
+        No incluye vasos, la foto es sólo ilustrativa.`
+    };
+
+    const interests = [
+        { name: 'Whiskey Blenders Pride 750ml', image: '/images/whisky.webp', points: '19.900' },
+        { name: 'Nombre del producto o servicio', image: '/images/fondo_blanco_negro.webp', points: 'XX.XXX' },
+        { name: 'Nombre del producto o servicio', image: '/images/fondo_blanco_negro.webp', points: 'XX.XXX' },
+        { name: 'Nombre del producto o servicio', image: '/images/fondo_blanco_negro.webp', points: 'XX.XXX' }
+    ];
+
+    res.render("pages/product", {
+        loggedIn: 1,
+        product,
+        interests
+    });
 });
 
 app.get("/cart", (req, res) => {
